bdb3fef7147452ebb36fa7db440906e04181427c
James Johnson <johnsojr@gmail.com>
2019-05-06 16:29:09 -0500

    show email total in cli

--- tasks/olCourseRegConfirm/index.js
+++ tasks/olCourseRegConfirm/index.js
@@ -70,10 +70,10 @@ async function sendEmails({ today }) {
     data,
     to: ({
       firstName, lastName, personalEmail, mcadEmail,
-    }) => `
-      ${firstName} ${lastName} <${personalEmail}>, 
-      ${firstName} ${lastName} <${mcadEmail}>
-    `,
+    }) => [
+      personalEmail ? `${firstName} ${lastName} <${personalEmail}>` : '',
+      mcadEmail ? `${firstName} ${lastName} <${mcadEmail}>` : '',
+    ].join(', '),
     from: () => 'MCAD Online Learning <online@mcad.edu>',
     bcc: () => 'MCAD Online Learning <online@mcad.edu>, ***REMOVED***',
   });
