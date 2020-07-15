53e52a3a526ce0ae8fa489df9031e005f73f66da
James Johnson <james_johnson@mcad.edu>
2020-07-15 12:06:47 -0500

    require username and mcadEmail for workshop confirmations

--- tasks/olWorkshopRegConfirm/index.test.js
+++ tasks/olWorkshopRegConfirm/index.test.js
@@ -4,17 +4,19 @@ const task = require('./index');
 describe('olWorkshopRegConfirm', () => {
   beforeEach(() => jest.clearAllMocks());
 
-  it.todo('generates workshop confirmation emails');
-
-  it('generates an error if a record is missing personal email', async () => {
+  it('generates an error if a record is missing MCAD email', async () => {
     jex.query = jest.fn().mockResolvedValue([
       {
         id: 1,
-        personalEmail: null,
+        username: 'user1',
+        mcadEmail: null,
+        personalEmail: '1@gmail.com',
       },
       {
         id: 2,
-        personalEmail: 'test@test.com',
+        username: 'user2',
+        mcadEmail: 'user2@mcad.edu',
+        personalEmail: '2@gmail.com',
       },
     ]);
     const { emails, errors } = await task({ today: '2019-01-22' });
@@ -22,10 +24,43 @@ describe('olWorkshopRegConfirm', () => {
     expect(errors).toMatchInlineSnapshot(`
       Array [
         "Error: Record does not have values in all required fields. Not generating email.
-      Required Fields: [personalEmail]
+      Required Fields: [username, mcadEmail]
       Record: {
         \\"id\\": 1,
-        \\"personalEmail\\": null
+        \\"username\\": \\"user1\\",
+        \\"mcadEmail\\": null,
+        \\"personalEmail\\": \\"1@gmail.com\\"
+      }",
+      ]
+    `);
+  });
+
+  it('generates an error if a record is missing username', async () => {
+    jex.query = jest.fn().mockResolvedValue([
+      {
+        id: 1,
+        username: null,
+        mcadEmail: 'user1@mcad.edu',
+        personalEmail: '1@gmail.com',
+      },
+      {
+        id: 2,
+        username: 'user2',
+        mcadEmail: 'user2@mcad.edu',
+        personalEmail: '2@gmail.com',
+      },
+    ]);
+    const { emails, errors } = await task({ today: '2019-01-22' });
+    expect(emails.length).toBe(1);
+    expect(errors).toMatchInlineSnapshot(`
+      Array [
+        "Error: Record does not have values in all required fields. Not generating email.
+      Required Fields: [username, mcadEmail]
+      Record: {
+        \\"id\\": 1,
+        \\"username\\": null,
+        \\"mcadEmail\\": \\"user1@mcad.edu\\",
+        \\"personalEmail\\": \\"1@gmail.com\\"
       }",
       ]
     `);
